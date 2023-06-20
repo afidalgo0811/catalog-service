5e4fab22fd95832f1a432459c1d7fb8bdb7bae1c
afidalgoSE <afidalgose@gmail.com>
2023-06-20 18:05:08 -0400

    added greetings function and HomeController

--- src/main/kotlin/com/afidalgo/catalogservice/CatalogServiceApplication.kt
+++ src/main/kotlin/com/afidalgo/catalogservice/CatalogServiceApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class CatalogServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<CatalogServiceApplication>(*args)
+    runApplication<CatalogServiceApplication>(*args)
 }
